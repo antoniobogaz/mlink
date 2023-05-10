d8ee21f4b224588e0dd46948d9e01ef625333711
Antonio Bogaz <antonioaugustobogaz@hotmail.com>
2023-05-09 23:07:00 -0300

    tela de login quase finalizada

--- lib/views/loginPage.dart
+++ lib/views/loginPage.dart
@@ -11,116 +11,140 @@ class loginPage extends StatefulWidget {
 }
 
 class _loginPageState extends State<loginPage> {
-  String logo = 'images/logo.jfif';
+  String logo = 'images/Logo_v3.jpg';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        scaffoldBackgroundColor: Color.fromARGB(255, 28, 28, 28),
-      ),
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                alignment: Alignment.center,
-                height: 500,
-                width: 300,
-                decoration: BoxDecoration(color: Colors.white),
-                child: Column(
-                  children: [
-                    Container(
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 3.5,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 139, 92, 235),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(80),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Spacer(),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      logo,
+                      height: 160,
+                      width: 160,
+                    ),
+                  ),
+                  //Spacer(),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 17, left: 32),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height / 2.4,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(top: 72),
+              child: Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.3,
+                    height: 50,
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Colors.black12, blurRadius: 5)
+                        ]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          icon: Icon(Icons.email, color: Colors.grey),
+                          hintText: 'Email'),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.3,
+                    height: 50,
+                    margin: EdgeInsets.only(top: 32),
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Colors.black12, blurRadius: 5)
+                        ]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          icon: Icon(Icons.key, color: Colors.grey),
+                          hintText: 'Senha'),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 12, right: 70),
+                      child: Text(
+                        'Esqueceu a senha?',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 1.3,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 139, 92, 235),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login'.toUpperCase(),
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  //Spacer(),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 5),
                       child: Center(
-                        child: Image.asset(
-                          logo,
-                          height: 250,
-                          width: 250,
+                        child: Text(
+                          'Não tem uma conta? Registre-se',
+                          style: TextStyle(color: Colors.grey, fontSize: 17),
                         ),
                       ),
                     ),
-                    Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(top: 0, right: 40, left: 10),
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              icon: Icon(Icons.person),
-                              hintText: 'Type your Email',
-                              labelText: 'E-mail',
-                            ),
-                            onChanged: (value) {},
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 0, right: 40, left: 10),
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              icon: Icon(Icons.key),
-                              hintText: 'Type your Password',
-                              labelText: 'Password',
-                            ),
-                            onChanged: (value) {},
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(top: 10, right: 0),
-                          child: ElevatedButton(
-                            child: Text('Login'),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => signInPage()));
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(top: 20, right: 0),
-                          child: TextButton(
-                            child:
-                                Text('I do not have an account, register now'),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => signInPage()));
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(top: 0, right: 0),
-                          child: TextButton(
-                            child: Text('Forgot my password'),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => signInPage()));
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ),
+            )
           ],
         ),
       ),
